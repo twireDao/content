9a63b7e4f8d15522852cc6fbc804f0c34ef87c12
Pulp <email@felixleber.de>
2022-10-10 22:31:59 +0200

    Add redeem endpoint

--- safebank/addWallet.js
+++ safebank/addWallet.js
@@ -38,3 +38,37 @@ $('form[action="https://safebank-backend.herokuapp.com/new"]').each(function (
         });
     });
 });
+
+$('form[action="https://safebank-backend.herokuapp.com/redeem"]').each(function (
+    i,
+    el
+) {
+    var form = $(el);
+    form.submit(function (e) {
+        e.preventDefault();
+        form = $(e.target);
+        var data = convertFormToJSON(form);
+        var action = form.attr("action");
+        $.ajax({
+            url: action,
+            method: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+            success: function () {
+                console.log("Success")
+                var parent = $(form.parent());
+                // Hide the form
+                parent.children("form").css("display", "none");
+                // Display the "Done" block
+                parent.children(".w-form-done").css("display", "block");
+            },
+            error: function () {
+                console.log("error")
+                var parent = $(form.parent());
+                // Display the "Failed" block
+                parent.find(".w-form-fail").css("display", "block");
+            },
+        });
+    });
+});
