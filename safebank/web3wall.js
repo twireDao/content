2c1ec10ee445f4bc16cc2f9b58fefbba6389eefa
Pulp <email@felixleber.de>
2022-10-15 19:33:41 +0200

    Add coinbase wallet

--- safebank/web3wall.js
+++ safebank/web3wall.js
@@ -52,6 +52,27 @@ function init() {
                 infuraId: INFURA_ID,
             }
         },
+        'custom-coinbase': {
+            display: {
+                logo: 'images/coinbase.svg',
+                name: 'Coinbase',
+                description: 'Scan with WalletLink to connect',
+            },
+            options: {
+                rpc: { 137: "https://matic-mainnet.chainstacklabs.com" },
+                infuraId: INFURA_ID,
+            },
+            package: WalletLink,
+            connector: async (_, options) => {
+                const { appName, networkUrl, chainId } = options
+                const walletLink = new WalletLink({
+                    appName
+                });
+                const provider = walletLink.makeWeb3Provider(networkUrl, chainId);
+                await provider.enable();
+                return provider;
+            },
+        }
 
 
     };
